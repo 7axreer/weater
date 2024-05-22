2be4ea896cdd9abe4ca9d73c5281a94949687261
axreer <saidaxrorpc7@gmail.com>
2024-05-22 18:41:33 +0500

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="nav">
+        <a href="" class="logo">
+            <img src="@/assets/img/logo.svg" alt="" />
+            <span>vue weather</span>
+        </a>
+        <div class="nav__search">
+            <img src="@/assets/img/kaplya.svg" alt="" />
+            <input type="text" v-model="city" 
+            placeholder="Выбрать город"/>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            city: "",
+        };
+    },
+};
+</script>
+
+<style></style>
