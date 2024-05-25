129fde50e21df836931b159b27f68ab6cd347360
axreer <saidaxrorpc7@gmail.comx>
2024-05-25 18:27:20 +0500

    changed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,20 +6,25 @@
         </a>
         <div class="nav__search">
             <img src="@/assets/img/kaplya.svg" alt="" />
-            <input type="text" v-model="city" 
+            <input type="text" 
+            v-model="city" 
+            @keydown.enter="getWeather(city), city = ''" 
             placeholder="Выбрать город"/>
         </div>
     </nav>
 </template>
 
 <script>
+
+import {mapActions} from 'vuex'
 export default {
     data() {
         return {
             city: "",
         };
     },
+    methods: {
+        ...mapActions(['getWeather'])
+    }
 };
 </script>
-
-<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,29 @@
-import { createStore } from 'vuex'
+import axios from "axios";
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    
-  },
-  getters: {
-    
-  },
-  mutations: {
-    
-  },
-  actions: {
-    
-  },
-})
+    state: {
+        apiKey: "65bdd06e9ec6b9ec751d2576d17e656e",
+        weather: null,
+    },
+    getters: {},
+    mutations: {},
+    actions: {
+        async getWeather(context, city) {
+            try {
+                let response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city},&appid=${context.state.apiKey}`);
+                let { lat, lon, local_names } = response.data[0];
+
+                if (local_names.en.toLowerCase() == city.toLowerCase() || local_names.ru.toLowerCase() == city.toLowerCase()) {
+                    let responseSec = await axios.get(
+                        `https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${context.state.apiKey}&units=metric&lang=ru`
+                    );
+                    console.log(responseSec.data.current.temp);
+                }
+                // console.log(response);
+            } catch (error) {
+                // console.log(error);
+            }
+        },
+    },
+});
